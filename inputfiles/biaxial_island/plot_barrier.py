bf184884779f374a9aa8e49df0e62e5455e0308f
Jonathan Maes <jonathan145b@hotmail.com>
2020-09-26 21:46:40 +0200

    rename

--- inputfiles/biaxial_island/plot_barrier.py
+++ inputfiles/biaxial_island/plot_barrier.py
@@ -17,13 +17,16 @@ if __name__ == "__main__":
     legend = ['Cubic']
     plt.plot(cubic["Angle"], cubic["E_total"])
 
-    # Shape anisotropy
-    shape = read_mumax3_table('biaxial_island_shape.out/table.txt')
+    # Shape anisotropy: Plus
+    shape = read_mumax3_table('biaxial_island_shape.out/tablePlus_70-100_0.03step.txt')
     for subset in shape.groupby("Length"):
         size = subset[0]
         subtable = subset[1]
+        E_demag = subtable["E_total"]-subtable["E_Zeeman"]
+        diff = max(E_demag) - min(E_demag)
+        print("(%.2f nm) Delta E: %.2e J = %.3f eV" % (size, diff, diff/1.602e-19))
         legend.append('Shape%d' % size)
-        plt.plot(subtable["Angle"], subtable["E_total"])
+        plt.plot(subtable["Angle"], E_demag)
     plt.legend(legend)
 
     # Show plot
